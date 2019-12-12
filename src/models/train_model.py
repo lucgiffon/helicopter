cafe10b9dfb2d1f530f12316ac97d7aa7dd1ff1d
Luc Giffon <luc.giffon@lis-lab.fr>
2019-12-12 18:11:45 +0100

    embed mdn training in clean functions

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -18,129 +18,84 @@ import matplotlib.pyplot as plt
 
 np.random.seed(42)
 
-# ---------------- Config params
 
-def learn_distribution(X, y):
-    """
-
-    :param X:
-    :param y:
-    :return: estimator
-    """
-
-    model = build_model(
-        loss=lambda y, f: gnll_loss(y, f),
-        input_shape=X.shape[1:],
-        n_neurons=16)
-
-    model.fit(
-        X_train,
-        y_train,
-        batch_size=batch_size,
-        epochs=n_epochs,
-        validation_data=(X_test, y_test),
-        verbose=1)
-
-    return model
-
-
-def gnll_loss(y, f):
-    """ Computes the negative log-likelihood loss of y given the parameters.
-    """
-    mu = f[:, 0]
-    sigma = f[:, 1]
-    gaussian_dist = tf.distributions.Normal(loc=mu, scale=sigma)
-    log_prob = gaussian_dist.log_prob(value=y)
-    neg_log_likelihood = -1.0 * K.sum(log_prob)
-    return neg_log_likelihood
-
-def build_model(loss, input_shape, n_neurons=16):
-    """builds the density network
-    """
+def build_base_network(input_shape):
     inputs = Input(shape=input_shape)
-    hidden = Dense(n_neurons)(inputs)
-    mixture_layer = mdn.MDN(1, 1)(hidden)
-    loss = mdn.get_mixture_loss_func(1, 1)
-    model = Model(inputs=inputs, outputs=mixture_layer)
-    model.compile(loss=loss, optimizer='nadam')
+    hidden = BatchNormalization()(inputs)
+    hidden = Dense(16)(hidden)
+    hidden = BatchNormalization()(hidden)
+    model = Model(inputs=inputs, outputs=hidden)
     return model
 
+def create_mdn_estimator(X, y, X_val, y_val, batch_size=32, nb_epochs=1000, nb_mixtures=1):
+    input_shape = X.shape[1:]
+    base_network = build_base_network(input_shape)
 
-if __name__ == "__main__":
+    input_a = Input(shape=input_shape)
+    x = base_network(input_a)
+    mixture_layer = mdn.MDN(1, nb_mixtures)(x)
+    mdn_model = Model([input_a], [mixture_layer])
+
+    loss = mdn.get_mixture_loss_func(1, nb_mixtures)
+    mdn_model.compile(loss=loss, optimizer='adam')
+
+    tb_cb = TensorBoard(log_dir='./mdn_logs/{}'.format(time.time()),
+                        histogram_freq=0, batch_size=32, write_graph=True, write_grads=True, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None,
+                        embeddings_data=None, update_freq='epoch')
+
+    mdn_model.fit(
+        X,
+        y,
+        batch_size=batch_size,
+        epochs=nb_epochs,
+        validation_data=(X_val, y_val),
+        verbose=1, callbacks=[tb_cb])
+
+    return mdn_model
+
+def make_mdn_prediction(mdn_model, X_test, return_sample=False, nb_mixture=1, scaling=1.):
+    assert nb_mixture == 1, "Only 1 mixture is supported"
+
+    y_pred = mdn_model.predict(X_test)
+    y_mu = y_pred[:, 0] * scaling
+    y_std = np.sqrt(y_pred[:, 1] * scaling ** 2)
+    if return_sample:
+        y_sample = np.apply_along_axis(sample_from_output, 1, y_pred, 1, nb_mixture, temp=1.0)
+        return y_mu, y_std, y_sample
+    else:
+        return y_mu, y_std
+
+def main():
     load_dotenv(find_dotenv())
     input_data_file = Path(os.environ["project_dir"]) / "data/external/pcm_main_rotor_embeded.npz"
     # input_data_file = Path(os.environ["project_dir"]) / "data/external/pcm_main_rotor.npz"
     data = np.load(input_data_file)
     X, y = data["X"], data["y"]
 
-    input_shape = X.shape[1:]
-    n_epochs = 1000
+    nb_epochs = 1000
     batch_size = 32
-    N_MIXES = 1
+    nb_mixtures = 1
 
     X = normalize(X, axis=0, norm="max")
-    # y = normalize(y[:, np.newaxis], axis=0, norm="max")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     max_y_train = np.max(y_train)
     y_train /= max_y_train
-    y_test /= max_y_train
-    # norm_y_train = np.linalg.norm(y_train)
-    # y_train /= norm_y_train
-    # y_test /= norm_y_train
+    X_val = X_test
+    y_val =  y_test / max_y_train
 
-    inputs = Input(shape=input_shape)
-    hidden = BatchNormalization()(inputs)
-    hidden = Dense(16)(hidden)
-    hidden = BatchNormalization()(hidden)
-    mixture_layer = mdn.MDN(1, N_MIXES)(hidden)
-    loss = mdn.get_mixture_loss_func(1, N_MIXES)
-    model = Model(inputs=inputs, outputs=mixture_layer)
-    model.compile(loss=loss, optimizer='adam')
+    model = create_mdn_estimator(X_train, y_train, X_val, y_val, batch_size=batch_size, nb_epochs=nb_epochs, nb_mixtures=nb_mixtures)
 
-
-    tb_cb = TensorBoard(log_dir='./mdn_logs/{}'.format(time.time()),
-                histogram_freq=0, batch_size=32, write_graph=True, write_grads=True , write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
-
-    model.fit(
-        X_train,
-        y_train,
-        batch_size=batch_size,
-        epochs=n_epochs,
-        validation_data=(X_test, y_test),
-        verbose=1, callbacks=[tb_cb])
-
-    y_pred = model.predict(X_test)
-    # y_samples = np.apply_along_axis(sample_from_output, 1, y_pred, 1, N_MIXES, temp=1.0)
-    y_samples = y_pred[:, 0]
-    y_std = np.sqrt(y_pred[:, 1] * max_y_train**2)
-    y_test *= max_y_train
-    y_samples *= max_y_train
-    print(np.mean(np.square(y_test-y_samples)))
-    print(np.linalg.norm(y_test - y_samples)/np.linalg.norm(y_test))
-    print(np.max(y_test), np.max(y_samples), np.max(np.abs(y_test-y_samples)))
+    y_mu_test, y_std_test = make_mdn_prediction(model, X_test, scaling=max_y_train)
+    y_true = y_test
     n_to_show = 100
-    plt.scatter(np.arange(y_test.shape[0])[:n_to_show], np.squeeze(y_test)[:n_to_show], label="True")
-    plt.errorbar(np.arange(y_test.shape[0])[:n_to_show], np.squeeze(y_samples)[:n_to_show], fmt='go', marker="o", ecolor="g", yerr=np.squeeze(y_std)[:n_to_show], label="Predicted")
+    plt.scatter(np.arange(y_true.shape[0])[:n_to_show], np.squeeze(y_true)[:n_to_show], label="True")
+    plt.errorbar(np.arange(y_true.shape[0])[:n_to_show], np.squeeze(y_mu_test)[:n_to_show], fmt='go', marker="o", ecolor="g", yerr=np.squeeze(y_std_test)[:n_to_show], label="Predicted")
     plt.legend()
     plt.show()
+    print(np.mean(np.square(y_true - y_mu_test)))
+    print(np.linalg.norm(y_true - y_mu_test) / np.linalg.norm(y_true))
+    print(np.max(y_true), np.max(y_mu_test), np.max(np.abs(y_true - y_mu_test)))
 
-    # learn_distribution(X_train, y_train)
 
-
-    y_pred = model.predict(X_train)
-    # y_samples = np.apply_along_axis(sample_from_output, 1, y_pred, 1, N_MIXES, temp=1.0)
-    y_samples = y_pred[:, 0]
-    y_std = np.sqrt(y_pred[:, 1] * max_y_train**2)
-    y_train *= max_y_train
-    y_samples *= max_y_train
-    print(np.mean(np.square(y_train-y_samples)))
-    print(np.linalg.norm(y_train - y_samples)/np.linalg.norm(y_train))
-    print(np.max(y_train), np.max(y_samples), np.max(np.abs(y_train-y_samples)))
-    n_to_show = 50
-    plt.scatter(np.arange(y_train.shape[0])[:n_to_show], np.squeeze(y_train)[:n_to_show], label="True")
-    plt.errorbar(np.arange(y_train.shape[0])[:n_to_show], np.squeeze(y_samples)[:n_to_show], fmt='go', marker="o", ecolor="g", yerr=np.squeeze(y_std)[:n_to_show], label="Predicted")
-    plt.legend()
-    plt.show()
-
-    # learn_distribution(X_train, y_train)
-
+if __name__ == "__main__":
+    main()
